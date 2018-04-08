719f86183c24881f57b476a586a9d426a8e1a10e
MrDreg0 <drak0on@yandex.ru>
2018-04-08 17:53:30 +0400

    add js
    - add js
    - add interactive map

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 var link = document.querySelector(".popup-link");
+var mapLink = document.querySelector(".main-contacts-map")
 var popup = document.querySelector(".popup");
+var popupMap = document.querySelector(".popup-map");
 var overlay = document.querySelector(".block-overlay");
 var closeButton = popup.querySelector(".close-popup");
+var closeMap = popupMap.querySelector(".close-popup");
 
 link.addEventListener('click', function(evt) {
 	evt.preventDefault();
 	popup.classList.add('show-popup');
 	overlay.classList.add('show-overlay');
+});
+
+mapLink.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	popupMap.classList.add("show-popup");
+	overlay.classList.add("show-overlay");
 });
 
 closeButton.addEventListener('click', function(evt) {
@@ -15,8 +24,15 @@ closeButton.addEventListener('click', function(evt) {
 	overlay.classList.remove('show-overlay');
 });
 
-overlay.addEventListener('click', function() {
-	popup.classList.remove('show-popup');
+closeMap.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	popupMap.classList.remove("show-popup");
+	overlay.classList.remove("show-overlay");
+})
+
+overlay.addEventListener('click', function(evt) {
+	evt.preventDefault();
+	popupMap.classList.remove("show-popup");
 	overlay.classList.remove('show-overlay');
 });
 
@@ -24,6 +40,14 @@ window.addEventListener('keydown', function(evt) {
 	if(evt.keyCode === 27 && popup.classList.contains('show-popup')) {
 		evt.preventDefault();
 		popup.classList.remove('show-popup');
+		overlay.classList.remove('show-overlay');
+	}
+});
+
+window.addEventListener('keydown', function(evt) {
+	if(evt.keyCode === 27 && popupMap.classList.contains('show-popup')) {
+		evt.preventDefault();
+		popupMap.classList.remove('show-popup');
 		overlay.classList.remove('show-overlay');
 	}
 });
